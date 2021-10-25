b45877d0e43baef0ca0d2dc51d390eb60364d51c
LouisaMedova <luiza.medova@phystech.edu>
2021-10-25 11:53:38 +0300

    corrections for contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 
-contract NFT is ERC721, ERC721Enumerable, ERC721URIStorage, AccessControl {
+contract NFT is ERC721Enumerable, ERC721URIStorage, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER");
     string private baseURI;
 
